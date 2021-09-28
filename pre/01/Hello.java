776cebe8ba3b7f5c2dc473401655f541e16796ec
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-09-27 21:59:59 -0400

    Update Hello.java

--- pre/01/Hello.java
+++ pre/01/Hello.java
@@ -1,3 +1,8 @@
+//<Perry> <Huang>
+//APCS
+//HW<05> -- <How Was Your Summer?/Summer Prework/Summer work + notes with teammates>
+//<2021>-<09>-<27>
+
 public class Hello{
   
       public static void main(String[] args){
