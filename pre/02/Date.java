4d882ce43847d048c2a1bfb324601a04cbcff87f
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-09-27 21:26:35 -0400

    Add files via upload

---
+++ pre/02/Date.java
@@ -0,0 +1,25 @@
+public class Date{
+      public static void main(String[] args){
+          System.out.println("Hello world!");
+          String day = "Monday";
+          String month = "December";
+          int date = 31;
+          int year = 2095;
+          System.out.println("American Format:");
+              System.out.print(day);
+              System.out.print(", ");
+              System.out.print(month);
+              System.out.print(" ");
+              System.out.print(date);
+              System.out.print(", ");
+              System.out.println(year);
+          System.out.println("European Format:");
+              System.out.print(day);
+              System.out.print(" ");
+              System.out.print(date);
+              System.out.print(" ");
+              System.out.print(month);
+              System.out.print(" ");
+              System.out.print(year);
+      }
+}
