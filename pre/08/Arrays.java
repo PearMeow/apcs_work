b96f4a82b7adf7ffa941f457aad43b56d2ce4bc7
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-09-28 21:22:31 -0400

    Add files via upload

---
+++ pre/08/Arrays.java
@@ -0,0 +1,21 @@
+public class Arrays{
+
+  public static double[] powArray(double[] a){
+    for (double i = 0; i < a.length; i++){
+
+    }
+    return a;
+  }
+
+      public static void main(String[] args){
+        double[] numbers;
+
+        for (int i = 0; i < 5; i++){
+          double j = i;
+          numbers[j] = j;
+
+        }
+      }
+
+}
+//* failure
