13b8b82b7b8c0bed3cc57b3a6a3062093cbd18f9
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-09-28 21:23:07 -0400

    Update Date.java

--- pre/03/Date.java
+++ pre/03/Date.java
@@ -1,3 +1,8 @@
+//<Perry> <Huang>
+//APCS
+//HW<05> -- <How Was Your Summer?/Summer Prework/Summer work + notes with teammates>
+//<2021>-<09>-<27>
+
 public class Date{
       public static void main(String[] args){
           System.out.println("Hello world!");
