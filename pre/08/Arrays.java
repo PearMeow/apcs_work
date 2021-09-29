5535f6493b463a3396560fb5ff31ffbe29c940a2
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-09-28 21:23:55 -0400

    Update Arrays.java

--- pre/08/Arrays.java
+++ pre/08/Arrays.java
@@ -1,3 +1,8 @@
+//<Perry> <Huang>
+//APCS
+//HW<05> -- <How Was Your Summer?/Summer Prework/Summer work + notes with teammates>
+//<2021>-<09>-<27>
+
 public class Arrays{
 
   public static double[] powArray(double[] a){
