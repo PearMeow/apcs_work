55f7b9aef9d38d4f8cb2bbc573d076ffeb8c3172
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-09-30 21:39:50 -0400

    Update Greet.java

--- 08_helloffxn/Greet.java
+++ 08_helloffxn/Greet.java
@@ -1,7 +1,14 @@
+//<Perry> <Huang>
+//APCS
+//HW<08> -- <Refactor Freshie Zero/Greeting People/Using a method inside the main method to greet people>
+//<2021>-<09>-<30>
+
 public class Greet{
 
     public static void main(String[] args) {
-        greet("hello");
+        greet("Good day to you, Salaj.");
+        greet("Hello to you too, Perry");
+        greet("What's up, Lawrence?");
   }
 
     static void greet(String str){
