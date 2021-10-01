6053c28b6b64e0c3b786397e94119ebd6240ce76
phuang30 <phuang30@stuy.edu>
2021-09-30 21:10:24 -0400

    init setup

--- 08_helloffxn/Greet.java
+++ 08_helloffxn/Greet.java
@@ -1,5 +1,11 @@
 public class Greet {
+
    public static void main(String[] args) {
       System.out.println("No hablo queso");
+        greet("Hello");
    } 
+   
+   static void greet(String str) {
+      System.out.println("hey");
+   }
 }
