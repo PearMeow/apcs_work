a054362f2152394af40c0dada986cbb84a07e700
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-01 21:29:19 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -2,8 +2,16 @@
 //APCS
 //HW<09> -- <What Are BigSibs Good For?/Greeting People/Using another file's methods to greet people in a file>
 //<2021>-<10>-<01>
-//<git add doesn't create a file, it adds the file to github for viewing.>
-//<You can make a file without a main method and you can use the contents of that file in other files.>
+
+//DISCOVERIES:
+//<git add doesn't create a file, it adds the file to github for viewing.
+//You can make a file without a main method and you can use the contents of that file in other files.>
+//UNRESOLVED QUESTIONS:
+//<Why do you need public in front of the class and the main method?
+//Are main methods actually called methods or are they called functions?
+//What does static do?
+//What is good style in java coding?>
+
 public class Greet {
 
    public static void main( String[] args ) {
