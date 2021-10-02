fecbf9ab608dd6188193e27d5c6fbc9d0c7c0f65
phuang30 <phuang30@stuy.edu>
2021-10-01 21:07:06 -0400

    init setup

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,6 +1,6 @@
 public class BigSib{
 
-    public static void main(String[] args) {
+    public static void greet(String str) {
         System.out.print("What's up ");
         System.out.print(str);
 	System.out.println("?");
