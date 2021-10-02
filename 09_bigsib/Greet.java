7fbb96fe90cceea90b3600815b5a93b96bf9b966
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-01 21:27:27 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -2,7 +2,8 @@
 //APCS
 //HW<09> -- <What Are BigSibs Good For?/Greeting People/Using another file's methods to greet people in a file>
 //<2021>-<10>-<01>
-
+//<git add doesn't create a file, it adds the file to github for viewing.>
+//<You can make a file without a main method and you can use the contents of that file in other files.>
 public class Greet {
 
    public static void main( String[] args ) {
