2fb96574bf336b18bfbabb1b3122f00f77575fc2
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-01 21:42:17 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,18 +1,19 @@
-//<Flaming Hot Cheetos - Perry Huang and Salaj Jr, Courtney Huang and Timothy, Samantha Hua and Poppy>
-//APCS
-//HW<09> -- <What Are BigSibs Good For?/Greeting People/Using another file's methods to greet people in a file>
-//<2021>-<10>-<01>
+/*
+<Flaming Hot Cheetos - Perry Huang and Salaj Jr, Courtney Huang and Timothy, Samantha Hua and Poppy>
+APCS
+HW<09> -- <What Are BigSibs Good For?/Greeting People/Using another file's methods to greet people in a file>
+<2021>-<10>-<01>
 
-//DISCOVERIES:
-//<git add doesn't create a file, it adds the file to github for viewing.
-//You can make a file without a main method and you can use the contents of that file in other files.>
+DISCOVERIES:
+<git add doesn't create a file, it adds the file to github for viewing.
+You can make a file without a main method and you can use the contents of that file in other files.>
 
-//UNRESOLVED QUESTIONS:
-//<Why do you need public in front of the class and the main method?
-//Are main methods actually called methods or are they called functions?
-//What does static do?
-//What is good style in java coding?>
-
+UNRESOLVED QUESTIONS:
+<Why do you need public in front of the class and the main method?
+Are main methods actually called methods or are they called functions?
+What does static do?
+What is good style in java coding?>
+*/
 public class BigSib{
 
     public static void greet(String str) {
