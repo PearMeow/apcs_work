cf08e128dc87d9af0730cf31a7c47b4f680a8be5
phuang30 <phuang30@stuy.edu>
2021-10-01 21:13:19 -0400

    init setup

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,14 @@
+//<Perry> <Huang>
+//APCS
+//HW<09> -- <What Are BigSibs Good For?/Greeting People/Using another file's methods to greet people in a file>
+//<2021>-<10>-<01>
+
+public class Greet {
+
+   public static void main( String[] args ) {
+       BigSib.greet("Salaj");
+       BigSib.greet("Perry");
+       BigSib.greet("Lawrence");
+       }
+
+}
