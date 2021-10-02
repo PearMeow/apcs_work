2ed33e0cec535f374d9c0ebf681b6704d3ed3cef
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-01 21:30:52 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -15,9 +15,9 @@
 public class Greet {
 
    public static void main( String[] args ) {
-       BigSib.greet("Salaj");
-       BigSib.greet("Perry");
-       BigSib.greet("Lawrence");
+       BigSib.greet("Flim");
+       BigSib.greet("Flam");
+       BigSib.greet("Kazaam");
        }
 
 }
