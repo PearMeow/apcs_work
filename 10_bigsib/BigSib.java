5845556df49cc0a247853c72845757a50e8350e5
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-04 17:40:02 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -1,7 +1,7 @@
 /*
 <Flaming Hot Cheetos - Perry Huang and Salaj Jr, Courtney Huang and Timothy, Samantha Hua and Poppy>
 APCS
-HW<10> -- <Refactor Big Sib One/Greeting People, but printing it/Using another file to generate strings of text for ><2021>-<10>-<04>
+HW<10> -- <Refactor Big Sib One/Greeting People, but printing it/Using another file to generate strings of text for this one to print><2021>-<10>-<04>
 DISCOVERIES:
 <You can add strings in java.
 You have to change the void part of a method to make it be able to return a type of value.>
