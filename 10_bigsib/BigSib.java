57428eb08b3cd084938127cba98caadd32016c7c
phuang30 <phuang30@stuy.edu>
2021-10-04 17:32:02 -0400

    hi

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -1,3 +1,15 @@
+/*
+<Flaming Hot Cheetos - Perry Huang and Salaj Jr, Courtney Huang and Timothy, Samantha Hua and Poppy>
+APCS
+HW<10> -- <Refactor Big Sib One/Greeting People, but printing it/Using another file to generate strings of text for ><2021>-<10>-<04>
+DISCOVERIES:
+<You can add strings in java.
+You have to change the void part of a method to make it be able to return a type of value.>
+
+UNRESOLVED QUESTIONS:
+<What values can be returned in a method?>
+*/
+
 public class BigSib{
 
    public static String greet(String str) {
--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,14 +1,14 @@
 /*
 <Flaming Hot Cheetos - Perry Huang and Salaj Jr, Courtney Huang and Timothy, Samantha Hua and Poppy>
 APCS
-HW<09> -- <Refactor Big Sib One/Greeting People, but printing it/Using another file to generate strings of text for this one to print>
+HW<10> -- <Refactor Big Sib One/Greeting People, but printing it/Using another file to generate strings of text for this one to print>
 <2021>-<10>-<04>
 DISCOVERIES:
 <You can add strings in java.
 You have to change the void part of a method to make it be able to return a type of value.>
 
 UNRESOLVED QUESTIONS:
-
+<What values can be returned in a method?>
 */
 
 public class Greet {
