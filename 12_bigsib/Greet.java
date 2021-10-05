0c6fffe4c42159b661bb044d41cf00458e085c78
phuang30 <phuang30@stuy.edu>
2021-10-05 19:37:26 -0400

    hi

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,7 +1,7 @@
 /*
 Perry Huang
 APCS
-HW12 -- <On Elder Individuality and the Elimination of Radio Fuzz//Summary>
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz/Greeting People/Making greetings but assigning BigSib to richard somehow
 2021-10-5
 DISCOS
 
@@ -19,5 +19,19 @@ public class Greet {
     greeting = richard.greet("freshman");
     System.out.println(greeting);
 
+    richard.setHelloMsg("Salutations");
+
+    greeting = richard.greet("Dr.Spaceman");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Hey ya");
+
+    greeting = richard.greet("Kong Fooey");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Sup");
+
+    greeting = richard.greet("mom");
+    System.out.println(greeting);
   } //end main()
 } //end Greet
