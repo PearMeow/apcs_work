5de1e48d7018e45243431a79f561ff25c88bdf84
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-05 19:59:16 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,12 +1,13 @@
 /*
-Perry Huang and Salaj Jr, Faiyaz Rafee and Pivot and Atlas
+Perry Huang
 APCS
-HW12 -- <On Elder Individuality and the Elimination of Radio Fuzz/Greeting People/Summary>
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz/Greeting People/Making greetings but assigning BigSib to richard somehow
 2021-10-5
 DISCOS
+BigSib richard = new BigSib() assigns BigSib() to richard which is why richard.methods* works.
 QCC
+Is there any way to print all of the required output without altering the Greet.java file?
 */
-
 public class BigSib{
 
    public static String setHelloMsg(String str) {
