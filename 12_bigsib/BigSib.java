3851d3fbfb446501dd452c29b6023e28d96bf00b
phuang30 <phuang30@stuy.edu>
2021-10-05 18:57:33 -0400

    hi

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,16 @@
+/*
+Perry Huang and Salaj Jr, Faiyaz Rafee and ...
+APCS
+HW12 -- <On Elder Individuality and the Elimination of Radio Fuzz//Summary>
+2021-10-5
+DISCOS
+QCC
+*/
+
+public class BigSib{
+
+   public static String greet(String str) {
+       return ("What's up " + str + "?");
+  }
+
+}
