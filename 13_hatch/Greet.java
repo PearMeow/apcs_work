bce557252a6fb476308a8c55a99b4aab4c1d1c55
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-06 23:06:04 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -13,8 +13,7 @@ public class Greet {
   public static void main( String[] args ) {
     String greeting;
 
-    BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
+    BigSib richard = new BigSib("Word up");
 
     greeting = richard.greet("freshman");
     System.out.println(greeting);
