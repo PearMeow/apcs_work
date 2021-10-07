c9bab7b6bb768220054532e883c0184f5bafb566
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-06 23:18:32 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -17,14 +17,6 @@ public class Greet {
 
     greeting = richard.greet("freshman");
     System.out.println(greeting);
-
-    greeting = richard.greet("Dr.Spaceman");
-    System.out.println(greeting);
-
-    greeting = richard.greet("Kong Fooey");
-    System.out.println(greeting);
-
-    greeting = richard.greet("mom");
-    System.out.println(greeting);
+    
   } //end main()
 } //end Greet
