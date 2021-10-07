8b5023ec1de7f9e141baf3a7871c66e62e1a31be
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-06 23:24:28 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -4,9 +4,10 @@ APCS
 HW13 -- Where do BigSibs Come From?/Greeting People/Making greetings but using a constructor
 2021-10-6
 DISCOS
-
+Constructors don't need static or a return value
+Every time new is run the constructor runs as well
 QCC
-
+How would you change the program so that the constructor can construct differently based on every greeting?
 */
 
 public class Greet {
