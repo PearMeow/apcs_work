de809e306a0d0da33dd7efa9e71dacef1e1c1bea
phuang30 <phuang30@stuy.edu>
2021-10-06 22:56:40 -0400

    hi

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,24 @@
+/*
+The Hotdogs: Perry Huang and Salaj Jr, Faiyaz Rafee and Pivot and Atlas
+APCS
+HW13 -- Where do BigSibs Come From?/Greeting People/Making greetings but using a constructor
+2021-10-6
+DISCOS
+
+QCC
+
+*/
+public class BigSib{
+
+   private BigSin{
+  }
+
+   public static String setHelloMsg(String str) {
+       System.out.print(str + " ");
+       return str;
+  }
+
+   public static String greet(String str) {
+       return str;
+  }
+}
---
+++ 13_hatch/Greet.java
@@ -0,0 +1,37 @@
+/*
+Perry Huang
+APCS
+HW13 -- Where do BigSibs Come From?/Greeting People/Making greetings but using a constructor
+2021-10-6
+DISCOS
+
+QCC
+
+*/
+
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Salutations");
+
+    greeting = richard.greet("Dr.Spaceman");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Hey ya");
+
+    greeting = richard.greet("Kong Fooey");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Sup");
+
+    greeting = richard.greet("mom");
+    System.out.println(greeting);
+  } //end main()
+} //end Greet
