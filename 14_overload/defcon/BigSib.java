571621011fa1786a77b78ff744e136ac84e1735c
phuang30 <phuang30@stuy.edu>
2021-10-07 21:07:49 -0400

    hi

---
+++ 14_overload/defcon/BigSib.java
@@ -0,0 +1,24 @@
+/* Perry Huang
+APCS
+HW14 -- Customize Your Creation/Overloading Constructors/Summary
+2021-10-07
+*/
+
+public class BigSib{
+
+   public BigSib() {
+      BigSib.setHelloMsg("Word up");
+  }
+
+   public String BigSib(String str) {
+      BigSib.setHelloMsg(str);
+  }
+
+   public static void setHelloMsg(String str) {
+       System.out.print(str + " ");
+  }
+
+   public static String greet(String str) {
+       return str;
+  }
+}
