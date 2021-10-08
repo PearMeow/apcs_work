0108eecfd87d91af23eaa25f4cb363ae28df5aa7
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-07 22:22:08 -0400

    Update BigSib.java

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -1,12 +1,13 @@
 /* Frogs on Fire: Perry Huang, Andrew Piatetsky, Samantha Hua
 APCS
-HW14 -- Customize Your Creation/Overloading Constructors/Summary
+HW14 -- Customize Your Creation/Overloading Constructors/Overloading constructors to customize greeting
 2021-10-07
 DISCOS
 Removing static makes it easier to change variable names without affecting the whole method.
 QCC
 What does removing static actually do to BigSib.java?
 */
+
 
 public class BigSib{
 
