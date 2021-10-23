480471cf44b6c67d8d3fbbd91cb011f2d6aca587
Perry Huang <67276046+PearMeow@users.noreply.github.com>
2021-10-23 12:03:49 -0400

    Update Coin.java

--- 23_Coin/Coin.java
+++ 23_Coin/Coin.java
@@ -1,31 +1,36 @@
-
-/***
-The Sentinels: Perry Huang and Salaj Rijal
+/*
+The Sentinels: Perry Huang, Salaj Rijal
 APCS
-HW23 -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
-2021-10-22
-time spent: 0.0 hrs
- ***/
+HW23 -- What Does Equality Look Like?/Making Coin.java and Driver.java/Flipping coins with bias.
+2021-10-23
+time spent: .9 hours
+ */
+
+import java.util.Random;
 
 public class Coin {
 
   //attributes aka instance vars
-  private double value;
-  private String upFace;
-  private String name; 
-  private int flipCtr;
-  private int tailsCtr;
-  private int headsCtr;
-  private float bias;
-
+  double value; //could possibly be float
+  String upFace = "heads";
+  String name;
+  int flipCtr;
+  int headsCtr;
+  int tailsCtr;
+  double bias = 0.5;
+  //could also possible be float
+  
+  //create random object
+  Random rng = new Random();
+	
+  
   /***
    *  Coin() -- default constuctor
    *  precond:
    *  postcond:
    ***/
-   
   public Coin() {
-    //stub
+
   }
 
 
@@ -40,8 +45,7 @@ public class Coin {
       "dollar"
       postcond:
   ***/
-  
-  public Coin(String s) {
+  public Coin( String s ) {
     name = s;
   }
 
@@ -52,32 +56,31 @@ public class Coin {
       postcond:
   ***/
   public Coin( String s, String nowFace ) {
-    name = s;
+		name = s;
     upFace = nowFace;
   }
-  
+
+
   // Accessors...
   // ----------------------------
   public String getUpFace() {
-  	return "";
-
+		return upFace;
   }
 
   public int getFlipCtr() {
-  	return 0;
-
+		return flipCtr;
   }
 
   public double getValue() {
-  	return 0.0;
+		return value;
   }
 
   public int getHeadsCtr() {
-  	return 0;
+		return headsCtr;
   }
 
   public int getTailsCtr() {
-  	return 0;
+		return tailsCtr;
   }
   // ----------------------------
 
@@ -89,8 +92,31 @@ public class Coin {
    * Returns value assigned.
    ***/
   private double assignValue( String s ) {
+		if ( s.equals("penny") ){
+			value = .01;
+      return value;
+    }
+		if ( s.equals("nickel") ){
+			value = .05;
+      return value;
+    }
+		if ( s.equals("dime") ){
+			value = .10;
+      return value;
+    }
+		if ( s.equals("quarter") ){
+			value = .25;
+      return value;
+    }
+		if ( s.equals("halfdollar") ){
+			value = .50;
+      return value;
+    }
+		if ( s.equals("dollar") ){
+			value = 1.0;
+      return value;
+    }
     return 0.0;
-    
   }
 
 
@@ -100,7 +126,14 @@ public class Coin {
       postcond: Coin's attribs reset to starting vals
   ***/
   public void reset( String s, double d ) {
-  
+  	double value = 0.0; //could possibly be float
+  	String upFace = "heads";
+  	String name = "";
+  	int flipCtr = 0;
+  	int headsCtr = 0;
+  	int tailsCtr = 0;
+  	double bias = 0.5; //could also possible be float
+
   }
 
 
@@ -114,7 +147,19 @@ public class Coin {
    * Returns "heads" or "tails"
    ***/
   public String flip() {
-  	return "";
+    double randomValue = rng.nextDouble();
+    if (randomValue > bias ){
+      flipCtr++;
+      tailsCtr++;
+      upFace = "tails";
+      return "tails";
+    }
+    else{
+      flipCtr++;
+      headsCtr++;
+      upFace = "heads";
+      return "heads";
+    }
   }
 
 
@@ -125,7 +170,8 @@ public class Coin {
    * or both showing tails. False otherwise.
    ***/
   public boolean equals( Coin other ) {
-  	return false;
+    return upFace.equals( other.getUpFace() );
+
   }
 
 
@@ -135,6 +181,60 @@ public class Coin {
    * postcond: Return String comprised of name and current face
    ***/
   public String toString() {
-	return upFace;
+		return name + " -- " + upFace;
   }
+
+}//end class
+
+
+//DRIVER FILE
+
+
+/***
+    driver for class Coin
+    ~~~ SUGGESTED WORKFLOW: ~~~
+    1. Compile this file and run. Note anything notable.
+    2. Move the "TOP" line down, so that it is below the first statement.
+    (emacs: with cursor at beginning of TOP line, C-k C-k, DOWN, DOWN, C-y)
+    (your editor: ???)
+    3. Compile and run again.
+    4. Resolve errors one at a time until it works.
+    5. Repeat 2-4 until TOP meets BOTTOM.
+***/
+
+public class Driver {
+
+  public static void main( String[] args ) {
+
+    //build Objects from blueprint specified by class Coin
+		
+
+    //test default constructor
+    //===================TOP==========================
+      Coin mine = new Coin();
+      //test 1st overloaded constructor
+      Coin yours = new Coin( "quarter" );
+      //test 2nd overloaded constructor
+      Coin wayne = new Coin( "dollar", "heads" );
+      //test toString() methods of each Coin
+      System.out.println("mine: " + mine);
+      System.out.println("yours: " + yours);
+      System.out.println("wayne: " + wayne);
+      //test flip() method
+      System.out.println("\nAfter flipping...");
+      yours.flip();
+      wayne.flip();
+      System.out.println("yours: " + yours);
+      System.out.println("wayne: " + wayne);
+      //test equals() method
+      if ( yours.equals(wayne) ) {
+        System.out.println( "Matchee matchee!" );
+      }
+      else {
+        System.out.println( "No match. Firestarter you can not be." );
+      }
+      //====================BOTTOM======================
+
+  }//end main()
+
 }//end class
