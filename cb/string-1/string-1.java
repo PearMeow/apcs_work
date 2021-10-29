17a69db4552a3d485532174cf191ca9f5de94442
phuang30 <phuang30@stuy.edu>
2021-10-29 13:19:08 -0400

    hi

--- cb/string-1/string-1.java
+++ cb/string-1/string-1.java
@@ -1,8 +1,13 @@
 public class string-1{
+
+public static void main(String[] args){
+  
+}
 public String helloName(String name) {
   return "Hello " + name + "!";
 }
 public String makeAbba(String a, String b) {
   return a + b + b + a;
 }
+
 }
