417c58c6320d5067beeeffbb6544bdf8522b1c90
PearMeow <phuang30@stuy.edu>
2021-10-29 22:35:09 -0400

    hi

--- cb/string-1/string1.java
+++ cb/string-1/string1.java
@@ -1,3 +1,32 @@
 public class string1 {
-    
+
+    public static String helloName(String name) {
+        return "Hello " + name + "!";
+      }
+
+    public static String makeAbba(String a, String b) {
+        return a + b + b + a;
+      }
+
+    public static void main(String[] args) {
+        System.out.println(helloName("Bob"));
+        System.out.println(helloName("Alice"));
+        System.out.println(helloName("X"));
+        System.out.println(helloName("Dolly"));
+        System.out.println(helloName("Alpha"));
+        System.out.println(helloName("Omega"));
+        System.out.println(helloName("ho ho ho"));
+        System.out.println(helloName("xyz"));
+        System.out.println(helloName("hello"));
+        System.out.println("------------------------");
+        System.out.println(makeAbba("Hi", "Bye"));
+        System.out.println(makeAbba("Yo", "Alice"));
+        System.out.println(makeAbba("What", "Up"));
+        System.out.println(makeAbba("aaa", "bbb"));
+        System.out.println(makeAbba("x", "y"));
+        System.out.println(makeAbba("x", ""));
+        System.out.println(makeAbba("", "y"));
+        System.out.println(makeAbba("Bo", "Ya"));
+        System.out.println(makeAbba("Ya", "Ya"));
+    }
 }
