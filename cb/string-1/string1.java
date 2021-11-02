5c2442927c0ede56e4ffd7d82efa4bb104f328ac
phuang30 <phuang30@stuy.edu>
2021-11-02 13:00:46 -0400

    hi

---
+++ cb/string-1/string1.java
@@ -0,0 +1,21 @@
+public class string1{
+
+public static void main(String[] args){
+  makeAbba("Hi", "Bye");
+  makeAbba("Yo", "Alice");
+  makeAbba("What", "Up");
+  makeAbba("aaa", "bbb");
+  makeAbba("x", "y");
+  makeAbba("x", "");
+  makeAbba("", "y");
+  makeAbba("Bo", "Ya");
+  makeAbba("Ya", "Ya");
+}
+public String helloName(String name) {
+  return "Hello " + name + "!";
+}
+public String makeAbba(String a, String b) {
+  return a + b + b + a;
+}
+
+}
