5423162b2bc03ae191240abc49c10db7f3a6933b
phuang30 <phuang30@stuy.edu>
2021-11-12 13:14:37 -0500

    hi

---
+++ 34_array/Loopier.java
@@ -0,0 +1,15 @@
+public class Loopier{
+
+  public static String arrayResult(int[] array) {
+  System.out.println(array);
+  }
+
+  public static int[] populateArray(int[] array) {
+  
+  for (int i = 0; i < array.length; i++) {
+  Math.random();
+  }
+  
+  }
+
+}
