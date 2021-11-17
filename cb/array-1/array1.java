8c3dd1f8a1db01fe2a4af048819bd82ff473740f
PearMeow <phuang30@stuy.edu>
2021-11-16 19:31:36 -0500

    hi

--- cb/array-1/array1.java
+++ cb/array-1/array1.java
@@ -1,21 +1,34 @@
 public class array1{
 
+/*Given an array of ints, return true if 6 appears as either the first or last element in the array.
+ The array will be length 1 or more.*/
+
   public static boolean firstLast6(int[] nums) {
   return (nums[0] == 6 || nums[nums.length - 1] == 6);
 }
 
+/*Given an array of ints, return true if the array is length 1 or more, and the first element and the
+ last element are equal.*/
+
   public static boolean sameFirstLast(int[] nums) {
   return (nums.length >= 1 && nums[0] == nums[nums.length - 1]);
 }
+
+//Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
 
   public static int[] makePi() {
   int[] array = {3, 1, 4};
   return array;
 }
 
+/*Given 2 arrays of ints, a and b, return true if they have the same first element or they have the
+  same last element. Both arrays will be length 1 or more.*/
+
   public static boolean commonEnd(int[] a, int[] b) {
   return (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]);
 }
+
+//Given an array of ints length 3, return the sum of all the elements.
 
   public static int sum3(int[] nums) {
   int result = 0;
