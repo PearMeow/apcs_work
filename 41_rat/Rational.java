dfb45b20a91d3ff8bc69f015fa20c6d11f70a7cc
PearMeow <phuang30@stuy.edu>
2021-12-01 17:32:16 -0500

    hi

--- 41_rat/Rational.java
+++ 41_rat/Rational.java
@@ -1,3 +1,9 @@
+/*
+Team BPK: Ben Belotser, Perry Huang, Kosta Dubovskiy
+APCS pd07
+hw41
+*/
+
 import javax.swing.JSpinner.NumberEditor;
 
 public class Rational {
@@ -10,16 +16,22 @@ public class Rational {
     }
 
     public Rational(int n, int d) {
+        if (d != 0) {
         numerator = n;
         denominator = d;
+        }
+        else {
+        numerator = 0;
+        denominator = 1;
+        }
     }
 
     public String toString() {
         return (numerator + "/" + denominator);
     }
 
-    public float floatVal() {
-        return (float)numerator / (float)denominator;
+    public double floatVal() {
+        return (double)numerator / (double)denominator;
     }
 
     public void multiplyBy(Rational Rat) {
@@ -33,6 +45,16 @@ public class Rational {
     }
 
     public static void main(String[] args) {
+        Rational r = new Rational(3, 6);
+        Rational s = new Rational(1, 2);
+        r.multiplyBy(s);
+        System.out.println(r.toString());
+        System.out.println(s.toString());
+        r.divideBy(s);
+        System.out.println(r.toString());
+        System.out.println(s.toString());
+        System.out.println(r.floatVal());
+        System.out.println(s.floatVal());
 
     }
 }
