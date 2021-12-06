1727f13354aa19c823fbf3a04473b19802645f54
PearMeow <phuang30@stuy.edu>
2021-12-05 19:58:44 -0500

    hi

--- 42_rat/Rational.java
+++ 42_rat/Rational.java
@@ -1,10 +1,9 @@
 /*
 Team BPK: Ben Belotser, Perry Huang, Kosta Dubovskiy
 APCS pd07
-HW41 -- Be Rational/Rationals/Making rationals and multiplying and dividing them
-2021-12-01
-time spent: 1.0 hour
-
+HW42 -- Be Rational/Rationals/Making rationals and adding/subtracting/comparing them
+2021-12-05
+time spent: 0.5 hour
 DISCO
 Using an object to change another object doesn't change the first object's value.
 QCC
@@ -56,22 +55,50 @@ public class Rational {
         return a;
       }
       else if (a > b) {
-        return gcdER(a-b, b);
+        return gcd(a-b, b);
       }
       else {
-        return gcdER(a, b-a);
+        return gcd(a, b-a);
       }
     }
 
-    public Rational reduce(Rational r) {
-
+    public void reduce() {
+      int numDenomGCD = gcd(numerator, denominator);
+      this.numerator /= numDenomGCD;
+      this.denominator /= numDenomGCD;
       
+    }
 
+    public void addToSelf(Rational Rat) {
+      // a / b + c / d = (ad + cb)/ bd
+      this.numerator *= Rat.denominator; // this numerator goes from a -> ad
+      this.numerator += Rat.numerator * this.denominator; // this numerator goes from ad -> ad + cb
+      this.denominator *= Rat.denominator; // this denominator goes from b -> bd
+    }
+
+    public void subtractFromSelf(Rational Rat) {
+      // a / b - c / d = (ad - cb)/ bd
+      this.numerator *= Rat.denominator; // this numerator goes from a -> ad
+      this.numerator -= Rat.numerator * this.denominator; // this numerator goes from ad -> ad - cb
+      this.denominator *= Rat.denominator; // this denominator goes from b -> bd
+    }
+
+    public int compareTo(Rational Rat) {
+      double callingObjVal = this.floatVal(); // easier to work w/ raw decimal value for comparisons than numerator / denominator
+      double paramObjVal = Rat.floatVal(); // same reasoning as before
+      if (callingObjVal > paramObjVal) {
+        return 1; // ours is larger than theirs
+      } else if (callingObjVal < paramObjVal) {
+        return -1; // ours is smaller than theirs
+      }
+      return 0; // equal
     }
 
     public static void main(String[] args) {
         Rational r = new Rational(3, 6);
         Rational s = new Rational(1, 2);
+        r.reduce();
+        System.out.println(r.toString());
         r.multiplyBy(s);
         System.out.println(r.toString());
         System.out.println(s.toString());
@@ -80,6 +107,9 @@ public class Rational {
         System.out.println(s.toString());
         System.out.println(r.floatVal());
         System.out.println(s.floatVal());
+        r.addToSelf(s);
+        System.out.println(r.toString());
+        System.out.println(s.toString());
 
     }
 }
