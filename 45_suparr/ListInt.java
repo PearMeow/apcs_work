cf55720c5899f3d131b5e45d442859f770f39323
PearMeow <phuang30@stuy.edu>
2021-12-08 21:48:47 -0500

    hi

--- 45_suparr/ListInt.java
+++ 45_suparr/ListInt.java
@@ -1,3 +1,11 @@
-public class ListInt {
+public interface ListInt {
+
+    public boolean add(int newVal);
+
+    public int add(int index, int newVal);
     
+    public int remove(int index);
+
+    public int size();
+
 }
