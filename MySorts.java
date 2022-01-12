d3ac1b7bb31f10c3a3fe940847d9f259abbf7dbd
PearMeow <phuang30@stuy.edu>
2022-01-11 20:52:50 -0500

    hgi

--- MySorts.java
+++ MySorts.java
@@ -1,46 +1,24 @@
 import java.util.ArrayList;
 
-public class MySorts {
-//HELPER METHODS
+public class MySorts{
 
-// returns an ArrayList of random integers from lo to hi, inclusive
-public static ArrayList<Integer> populate( int size, int lo, int hi ) {
-    ArrayList<Integer> retAL = new ArrayList<Integer>();
-    while( size > 0 ) {
-      //     offset + rand int on interval [lo,hi]
-      retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
-      size--;
-    }
-    return retAL;
-  }
-  
-  //randomly rearrange elements of an ArrayList
-  public static void shuffle( ArrayList al )
-  {
-    int randomIndex;
-    //setup for traversal fr right to left
-    for( int i = al.size()-1; i > 0; i-- ) {
-      //pick an index at random
-      randomIndex = (int)( (i+1) * Math.random() );
-      //swap the values at position i and randomIndex
-      al.set( i, al.set( randomIndex, al.get(i) ) );
-    }
-  }
-  
   //swaps items in the 2 indexes and returns the new ArrayList
   public static void swap(ArrayList swapee, int i1, int i2){
     Object temp = swapee.get(i1);
     swapee.set(i1, swapee.get(i2) );
     swapee.set(i2, temp);
   }
-  
-  // Rearranges elements of input ArrayList
-  // postcondition: data's elements sorted in ascending order
+
+  /*
+  ALGO: 
+  0. Start at right most index.
+  1. Looks at current value and compares it with the adjacent left value.
+  2. Swap if the right value is less than the left value. 
+  3. Watch me whip whip, nae nae..... Move the index to the left. 
+  4. Continue from step 2 unless there is no left value, in which case continue from step 1.
+  */
   public static void bubble( ArrayList<Comparable> data )
   {
-    int comparisons = 0;
-    int swaps = 0;
-    int passes = 0;
     //for each pass do this
     //we loop size of array - 1 times
     for( int i = 0; i < data.size()-1; i++){
@@ -51,65 +29,61 @@ public static ArrayList<Integer> populate( int size, int lo, int hi ) {
         Comparable right = data.get(x);
         //check right is greater than or = to left
         if( right.compareTo(left) >= 0 ){
-          comparisons++;
           continue;
         }
-        comparisons++;
-        //swap
         swap(data, x, x-1);
-        swaps++;
       }
-      passes++;
     }
   }
-  
+
+  /*
+  ALGO:
+  0. Start at right most index. 
+  1. Compare with every value to the left, including the start.
+  2. Swap with the least value.
+  3. Move index to left by 1.
+  4. Go back to step 1.
+  */
   public static void selection(ArrayList<Comparable> data )
-    {
-      int comparisons = 0;
-      int swaps = 0;
-      int passes = 0;
-      //note: this version places greatest value at "rightmost" end
-      //maxPos will point to position of SELECTION (greatest value)
-      int maxPos;
-  
-      for(int pass = data.size() - 1; pass >= 0; pass--) {
-        maxPos = 0;
-        for(int i = 0; i <= pass; i++) {
-          if (data.get(i).compareTo(data.get(maxPos)) > 0) {
-            maxPos = i;
-          }
-          comparisons++;
+  {
+    //maxPos will point to position of SELECTION (greatest value)
+    int maxPos;
+    for(int pass = data.size() - 1; pass >= 0; pass--) {
+      maxPos = 0;
+      for(int i = 0; i <= pass; i++) {
+        if (data.get(i).compareTo(data.get(maxPos)) > 0) {
+          maxPos = i;
         }
-        swap(data, pass, maxPos);
-        swaps++;
-        passes++;
       }
-    }//end selectionSort
+      swap(data, pass, maxPos);
+    }
+  }//end selectionSort
+
   
-    // Rearranges elements of input ArrayList
-    // postcondition: data's elements sorted in ascending order
-    public static void insertion( ArrayList<Comparable> data )
-    {
-      int comparisons = 0;
-      int swaps = 0;
-      int passes = 0;
-      for(int partition = 0; partition < data.size() - 1; partition++) {
-        //partition marks first item in unsorted region
-        //traverse sorted region from right to left
-        for(int i = partition + 1; i > 0; i--) {
-          // "walk" the current item to where it belongs
-          // by swapping adjacent items
-          if ( data.get(i).compareTo(data.get(i - 1)) < 0 ) {
-            swap(data, i, i - 1);
-            swaps++;
-          }
-          else{
-            comparisons++;
-            break;
-          }
-          comparisons++;
+  /*
+  ALGO:
+  0. Create a partition, starting at index 0.
+  1. Make the value to the left of the end of the partition the "new_value".
+  2. If the new_value is less than the value to the right, swap the two.
+  3. Repeat step 2 until the new_value is in the correct position inside the partition.
+  4. Increase the size of the parition by 1 and start at step 1.
+  */
+  public static void insertion( ArrayList<Comparable> data )
+  {
+    for(int partition = 0; partition < data.size() - 1; partition++) {
+      //partition marks first item in unsorted region
+      //traverse sorted region from right to left
+      for(int i = partition + 1; i > 0; i--) {
+        // "walk" the current item to where it belongs
+        // by swapping adjacent items
+        if ( data.get(i).compareTo(data.get(i - 1)) < 0 ) {
+          swap(data, i, i - 1);
         }
-        passes++;
+        else{
+          break;
+        }
       }
-    }//end insertionSort
-}
+    }
+  }//end insertionSort
+
+}//end of class
