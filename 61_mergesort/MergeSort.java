02b3baa4da0ab8650cbb4f3256c77d8b2d91960e
PearMeow <phuang30@stuy.edu>
2022-02-08 18:36:48 -0500

    hi

--- 61_mergesort/MergeSort.java
+++ 61_mergesort/MergeSort.java
@@ -3,7 +3,20 @@
   Implements mergesort on array of ints.
 
   Summary of Algorithm:
-
+  merge:
+  * Initialize 2 counters that are equal to 0, one for each array.
+  * Given 2 arrays, compare the values in the index of the counter in both of them.
+  * Add lower value to final array.
+  * Increment counter of the array of the lower value by 1.
+  * Keep comparing until the end of one of the arrays has been reached.
+  * Add all remaining values of the other array.
+  * Return the final array.
+  sort:
+  * Initialize 2 arrays with sizes of 1 that have the first and second value of the input array.
+  * In a for loop, merge the two arrays and set the result equal to the first array by using a temporary array.
+  * Also in the for loop, set the second array's value equal to the next value in the given array.
+  * Keep merging the two arrays until you reach the end of the given array.
+  * Return the first array.
   ***/
 
 public class MergeSort
@@ -17,21 +30,33 @@ public class MergeSort
    ******************************************************/
   private static int[] merge( int[] a, int[] b )
   {
-    int finalCounter = 0;
     int aCounter = 0;
     int bCounter = 0;
     int[] finalArray = new int[a.length + b.length];
-    while (aCounter < a.length && bCounter < b.length) {
-      if (a[aCounter] < b[bCounter]) {
+    for (int finalCounter = 0; finalCounter < (a.length + b.length); finalCounter++) {
+      if (bCounter == b.length) {
         finalArray[finalCounter] = a[aCounter];
-        finalCounter++;
-      }
-      else {
+        aCounter++;
+        }
+      else if (aCounter == a.length) {
         finalArray[finalCounter] = b[bCounter];
-        finalCounter++;
-      }
+        bCounter++;
+        }
+      else {
+        
+        if (a[aCounter] < b[bCounter]) {
+          finalArray[finalCounter] = a[aCounter];
+          aCounter++;
+        }
+        else {
+          finalArray[finalCounter] = b[bCounter];
+          bCounter++;
+        }
 
+      }
     }
+  
+    return finalArray;
   }//end merge()
 
 
@@ -42,7 +67,16 @@ public class MergeSort
    ******************************************************/
   public static int[] sort( int[] arr )
   {
-
+    int[] firstArray = new int[1];
+    firstArray[0] = arr[0];
+    int[] secondArray = new int[1];
+    for (int i = 0; i < arr.length - 1; i++) { 
+      secondArray[0] = arr[i + 1];
+      int[] tempArray = merge(firstArray, secondArray);
+      firstArray = tempArray;
+    }
+    return firstArray;
+    
   }//end sort()
 
 
@@ -68,7 +102,6 @@ public class MergeSort
   //main method for testing
   public static void main( String [] args )
   {
-    /*~~~~~~~~~~~~~~ Ye Olde Tester Bar ~~~~~~~~~~~~~~
       int[] arr0 = {0};
       int[] arr1 = {1};
       int[] arr2 = {1,2};
@@ -86,15 +119,17 @@ public class MergeSort
       System.out.println("\nMerging arr1 and arr0: ");
       printArray( merge(arr1,arr0) );
 
-      System.out.println("\nMerging arr4 and arr6: ");
-      printArray( merge(arr4,arr6) );
-
       System.out.println("\nSorting arr4-7...");
       printArray( sort( arr4 ) );
       printArray( sort( arr5 ) );
       printArray( sort( arr6 ) );
       printArray( sort( arr7 ) );
-      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+      System.out.println("\nMerging arr4 and arr6: ");
+      printArray( merge(sort(arr4),sort(arr6)) );
+
+
+
   }//end main()
 
 }//end class MergeSort
