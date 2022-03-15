8b237f1d8febbb09e99d239f26a294392ff65866
PearMeow <phuang30@stuy.edu>
2022-03-14 22:53:12 -0400

    hi

--- 76_llist/LList.java
+++ 76_llist/LList.java
@@ -1,4 +1,4 @@
-//Pom Pom: Max Schneider, Perry Huang, Oscar Breen
+///Pom Pom: Max Schneider, Perry Huang, Oscar Breen
 //APCS pd07
 //HW76 -- We Got a Little Ol' Convoy
 //2022-03-14
@@ -28,25 +28,45 @@ public class LList implements List //interface def must be in this dir
 
   public boolean add( String newVal )
   {
-    _head.setNext(newVal, null);
+    LLNode temp = _head;
+
+//   _head.setCargo(newVal);
+     while(temp.getNext() != null){
+	temp = temp.getNext();
+     }
+     LLNode val = new LLNode(newVal, null);
+      temp.setNext(val);
+     return true;
   }
 
 
   public String get( int index )
   {
+    String ind = "";
     if ( index < 0 || index >= size() )
       throw new IndexOutOfBoundsException();
-
-    return "bruh";
-  }
+      if (index == 0)
+	ind = (_head.getCargo());
+     else
+	for(int i = 0; i <= index; i++){
+		ind = _head.getCargo();
+		_head = _head.getNext();
+	}
+    return ind;
+} 
 
 
   public String set( int index, String newVal )
   {
-
+//    String res = "";
     if ( index < 0 || index >= size() )
       throw new IndexOutOfBoundsException();
-
+     LLNode boc = _head;
+     for( int i = 0; i < index; i++){
+     	boc = boc.getNext();
+     }
+     boc.setCargo(newVal);
+    return newVal;
     // YOUR CODE HERE
   }
 
@@ -54,7 +74,7 @@ public class LList implements List //interface def must be in this dir
   //return number of nodes in list
   public int size()
   {
-    int i = 0
+    int i = 0;
     LLNode temp = _head;
     while( temp != null ) {
       System.out.println( temp );
@@ -66,19 +86,18 @@ public class LList implements List //interface def must be in this dir
 
   //--------------^  List interface methods  ^--------------
 
+/*
 
-
-  // override inherited toString
+  override inherited toString
   public String toString()
   {
     // YOUR CODE HERE
   }
-
-
+*/
   //main method for testing
   public static void main( String[] args )
   {
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     LList james = new LList();
 
     System.out.println( james );
@@ -106,7 +125,6 @@ public class LList implements List //interface def must be in this dir
     System.out.println( "...and now 2nd item is: " + james.set(1,"got") );
 
     System.out.println( james );
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }
 
 }//end class LList
