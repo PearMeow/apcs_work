d436c98759d98193dcc81127ff1c7084a0c00a54
phuang30 <phuang30@stuy.edu>
2022-04-05 13:18:55 -0400

    hi

--- cobo/data/Cereal.java
+++ cobo/data/Cereal.java
@@ -1,7 +1,14 @@
+/*
+Team LPL: Lawrence Joa, Perry Huang, Lior Polischouk
+APCS
+LAB08 -- America's Next Top Data Scientist
+2022-04-05
+time spent: 9999 hrs
+*/
 public class Cereal {
 
     public Cereal () {
-        
+
     }
 
 }
--- cobo/data/README.md
+++ cobo/data/README.md
@@ -1,3 +1,9 @@
+Team LPL: Lawrence Joa, Perry Huang, Lior Polischouk
+APCS
+LAB08 -- America's Next Top Data Scientist
+2022-04-05
+time spent: 9999 hrs
+
 Activity 1:
 1. Youtube, GMail
 2. Username, password, email
