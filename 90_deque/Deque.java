ec47560547a3dc6b8df9fc1790e7d2025c791bd2
PearMeow <phuang30@stuy.edu>
2022-04-12 20:36:53 -0400

    Hi

--- 90_deque/Deque.java
+++ 90_deque/Deque.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public interface Deque<T> 
 {
 
@@ -9,27 +7,7 @@ public interface Deque<T>
     public T pollLast();
     public T peekFirst();
     public T peekLast();
+    public boolean offerFirst();
+    public boolean offerLast();
     
-}
-
-public class QQCC<T> implements Deque<T> 
-
-{
-    private ArrayList<T> _data = new ArrayList<T>();
-    private int _first = 0;
-    private int _last = 0;
-
-    public void addFirst(T newVal) {
-        _data.addFirst(newVal);
-        _last++;
-    }
-    public void addLast(T newVal) {
-        _data.addLast(newVal);
-        _last++;
-    }
-
-    public T pollFirst() {
-        
-    }
-
 }
