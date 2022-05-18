8d3abe6b5fdfb6861f512184a9178a905b05489a
phuang30 <phuang30@stuy.edu>
2022-05-18 12:45:31 -0400

    hi

---
+++ 103_/RunMed.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class RunMed {
+    
+    new ArrayList<Integer> max;
+    new Integer median;
+
+    public RunMed {
+        max = new ArrayList<Integer>(); 
+        median = 0;        
+    }
+
+    public Integer getMedian() {
+        return median;
+    }
+
+    public Integer add(Integer newVal) {
+        max.add(newVal);
+    }
+
+}
