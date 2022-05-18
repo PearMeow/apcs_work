e0b3e64811b28effd6648f62cb23d17cd76607e1
PearMeow <phuang30@stuy.edu>
2022-05-18 18:56:14 -0400

    hi

---
+++ 103_runmed/RunMed.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class RunMed {
+
+    public RunMed() {
+
+    }
+
+    public Integer getMedian() {
+        return 0;
+    }
+
+    public Integer add(Integer newVal) {
+        return 0;
+    }
+
+}
